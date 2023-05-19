d3fd46b405a79a7839a50b11fdd76aa66ff5850d
mfestini2 <mfestini@dc.uba.ar>
2023-05-19 17:35:09 -0300

    Agregada insertar con compare exchange

--- src/ListaAtomica.hpp
+++ src/ListaAtomica.hpp
@@ -32,14 +32,43 @@ class ListaAtomica {
 
     void insertar(const T &valor) {
         // Completar (Ejercicio 1)
+        
         Nodo *nuevo_nodo = new Nodo(valor); //Estoy asumiendo que new no tiene problemas de concurrencia
 
+        /*{ // FORMA 1 (MUTEX)
         _lock_lista.lock();
 
         nuevo_nodo->_siguiente = _cabeza.load();
         _cabeza.store(nuevo_nodo);
         
         _lock_lista.unlock();
+        }*/
+
+        { // FORMA 2 (COMPARE EXCHANGE)
+        nuevo_nodo->_siguiente = _cabeza.load();
+        while(!_cabeza.compare_exchange_weak(nuevo_nodo->_siguiente, nuevo_nodo));
+        // realiza ATOMICAMENTE la comparacion entre nuevo_nodo->_siguiente y _cabeza.
+        // Si la comparacion devuelve verdadero la operacion retorna verdadero y reemplaza el valor de _cabeza
+        // por el valor de nuevo_nodo
+        // Si la comparacion devuelve falso (porque se modifico el valor de _cabeza durante la ejecucion)
+        // lee en _cabeza el nuevo valor e intenta nuevamente la comparacion con los valores actualizados
+
+        // Existe compare_exchange weak y strong, segun entendi, si te cambian la variable antes de realizar
+        // esta operacion atomica, la comparacion va a fallar y por lo tanto la funcion va a fallar.
+        // Si el compare es strong, el procesador es el responsable de volver a intentar la funcion hasta
+        // que salga bien y esto puede llegar a ser medio lento
+        // Si el compare es weak, somos nosotros los responsables de volver a intentar hacer la funcion
+
+        // Segun la pagina que me explico como funcionaba la funcion, para estructuras "lock free" como la
+        // nuestra en este caso conviene usar compare weak
+
+        // https://stackoverflow.com/questions/4944771/stdatomic-compare-exchange-weak-vs-compare-exchange-strong
+        // Explicacion de las diferencias entre weak y strong
+
+        // https://blog.devgenius.io/a-simple-guide-to-atomics-in-c-670fc4842c8b
+        // Este explica como usar algunas variables atomicas incluyendo el compare exchange y sugiere usar weak
+        // para este tipo de estructuras
+        }
         // Si se nos ocurre una idea con variables atomicas podemos usar ambas y compararlas en el informe
     }
 
