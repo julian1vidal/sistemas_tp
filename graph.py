32d314151a5ffc5d9c87f1cb0c3a8af75033db95
alansolow <88858083+alansolow@users.noreply.github.com>
2023-06-09 14:33:59 -0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,20 +1,63 @@
 import matplotlib.pyplot as plt
+import os
 
-# max_par = [455507, 509800, 589489, 633433, 786030, 1074112, 1514533, 2649454]
-# max = [70364, 97535, 174015, 383088, 706967, 1421388, 2560130, 5537886]
-# max_par = [434238, 488550, 434757, 451127, 543919, 501475, 551005, 477098]
-# max = [4631, 5681, 6798, 13977, 21125, 51090, 67636, 169717]
+def plotGraph():
+    
 
-x = [256, 512, 1024, 2048, 4096, 8192, 16384, 32768]
+    if os.path.isfile("maximoThreads.txt") == True:
+        f = open("maximoThreads.txt", "r")
+        line = f.readline()
+        f.close()
+        values = line.split("--")
+        values = values[:-1]
+        values = [i.split("-") for i in values]
+        print(values)
+        values_max = [values[i][0] for i in range(len(values))]
+        std = [values[i][1] for i in range(len(values)) ]
+        values_max = [float(i) for i in values_max]
+        std = [float(i) for i in std]
+        X = range(1, 27)
+        
+        plt.errorbar(X, values_max, yerr=std ,fmt='-bo')
+        plt.xlabel("Cantidad de threads")
+        plt.ylabel("Tiempo de ejecucion (ns)")
+        plt.show()
+        #os.remove("maximoThreads.txt")
 
-#plot max vs max_par in dots united by lines
-plt.plot(x, max_par, 'ro-', label='max_par')
-plt.plot(x, max, 'bo-', label='max')
-plt.legend()
-plt.xlabel('Words per list')
-plt.ylabel('Time (nano seconds)')
-plt.yscale('log')
-plt.title('Max vs Max_par (letra a)')
-plt.show()
+    else:
+        #Read from maximo.txt
+        f = open("maximo.txt", "r")
+        line = f.readline()
+        f.close()
+        values = line.split("--")
+        values = values[:-1]
+        values = [i.split("-") for i in values]
+        print(values)
+        values_max = [values[i][0] for i in range(len(values))]
+        std = [values[i][1] for i in range(len(values)) ]
+        #Read from maximoParalelo.txt
+        f = open("maximoParalelo.txt", "r")
+        line = f.readline()
+        f.close()
+        valuesParalelo = line.split("--")
+        valuesParalelo = valuesParalelo[:-1]
+        valuesParalelo = [i.split("-") for i in valuesParalelo]
+        valuesParalelo_max = [valuesParalelo[i][0] for i in range(len(valuesParalelo))]
+        stdParalelo = [valuesParalelo[i][1] for i in range(len(valuesParalelo)) ]
+        values_max = [float(i) for i in values_max]
+        std = [float(i) for i in std]
+        valuesParalelo_max = [float(i) for i in valuesParalelo_max]
+        stdParalelo = [float(i) for i in stdParalelo]
+        #Plot
+        X = [16*2**i for i in range(10)]
+        
+        plt.errorbar(X, values_max, yerr=std ,fmt='-bo', label="Secuencial")
+        plt.errorbar(X, valuesParalelo_max, yerr=stdParalelo ,fmt='-ro', label="Paralelo")
+        plt.xlabel("Palabras en el hashmap en todas las letras")
+        plt.ylabel("Tiempo de ejecucion (ns)")
+        plt.legend()
+        plt.show()
+        os.remove("maximo.txt")
+        os.remove("maximoParalelo.txt")
 
-
+plotGraph()
